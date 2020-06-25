f0aa9cb0ca0967263e3cfb5002ad8142319196d3
Meredith Reece <mardebugger@gmail.com>
2020-06-25 22:53:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ function init() {
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+let recipe = {
+  description: 'yummy chicken noodle soup',
+  ingredients: [
+    {quantity: "1 cup", name: 'chicken'},
+    {quantity: "3 nanoliters", name: 'stock'},
+    {quantity: "12", name: 'noodles'}
+  ]
+}
+
+let template = document.getElementById("my-template").innerHTML;
+let templateFn = Handlebars.compile(template);
+let html = templateFn(recipe);
